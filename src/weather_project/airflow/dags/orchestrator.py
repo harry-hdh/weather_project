18f5dfdbf17a650d016ad69727ecb2ed61afd86f
harry-hdh <hdh997@outlook.com>
2026-05-27 23:25:55 +1200

    create DAG for dbt

--- src/weather_project/airflow/dags/orchestrator.py
+++ src/weather_project/airflow/dags/orchestrator.py
@@ -1,6 +1,8 @@
 import sys
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
 from datetime import datetime, timedelta
 
 sys.path.append('/opt/airflow/api-request')
@@ -11,7 +13,7 @@ def main_callable():
 
 default_args = {
     'description':'A DAG to orchestrate data',
-    'start_date':datetime(2026,5,26),
+    'start_date':datetime(2026,5,27),
     'catchup':False
 }
 
@@ -26,4 +28,23 @@ with dag:
         task_id='insert_data_task',
         python_callable=main_callable
     )
-    #task2
+    
+    task2 = DockerOperator(
+        task_id='transform_data_task',
+        image='ghcr.io/dbt-labs/dbt-postgres:1.9.latest',
+        command='run',
+        working_dir='/usr/app',
+        mounts=[
+            Mount(source = '/home/hdh/weather_project/src/weather_project/dbt/my_project',
+                 target='/usr/app',
+                 type='bind'),
+            Mount(source = '/home/hdh/weather_project/src/weather_project/dbt/profiles.yml',
+                 target='/root/.dbt/profiles.yml',
+                 type='bind')
+        ],
+        network_mode='weather_project_my-network',
+        docker_url='unix://var/run/docker.sock',
+        auto_remove='success'
+    )
+
+    task1 >> task2
